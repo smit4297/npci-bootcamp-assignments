75a54d6d4eaa3dbbae9354c3515ca08493d62bcf
smit4297 <smit4297>
2022-07-30 00:35:53 +0530

    angular sales src uploaded

---
+++ Angular-Sales-Src-Files/src/app/sales-person-list/sales-person-list.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { SalesPerson } from './salesperson';
+
+@Component({
+  selector: 'app-sales-person-list',
+  templateUrl: './sales-person-list.component.html',
+  styleUrls: ['./sales-person-list.component.css']
+})
+export class SalesPersonListComponent implements OnInit {
+
+  name : string = "Smit";
+  s1 : SalesPerson = new SalesPerson("Smit","patel","smitpatel4297@gmail.com",40000,300);
+
+  salesPersonList:SalesPerson[] = [this.s1, new SalesPerson("nevya","patel","nevya@gmail.com",70000,1000)
+,new SalesPerson("Henish","patel","henisg@gmail.com",30000,100),
+new SalesPerson("Vaishvi","patel","vaishu@gmail.com",90000,3500)];
+  constructor() { }
+
+  salesPersonModel : SalesPerson = new SalesPerson ("", "", "", 0,0);
+   onSubmit(salesPersonModel : SalesPerson) { 
+     console.log(this.salesPersonModel);
+     this.salesPersonList.push(salesPersonModel);
+   }
+  ngOnInit(): void {
+  }
+
+}
